4b67978319bd47d20d593221065c38810a9b861f
naraeha <dkfmrhs170@gmail.com>
2023-07-17 10:53:17 +0900

    Update Gugudan.java

--- src/multiplication_table/Gugudan.java
+++ src/multiplication_table/Gugudan.java
@@ -51,20 +51,51 @@ public class Gugudan {
 //		result = 4 * 9;
 //		System.out.println(result);
 //		System.out.println();
-		
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("number : " + number);
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+//		
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println("number : " + number);
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
+//		
+//		System.out.println("6단");
+//		int i = 1;
+//		while (i< 10) {
+//			System.out.println(6*i);
+//			i = i+1;
+//		}
+//		
+//		System.out.println();
+//		
+//		System.out.println("7단");
+//		for (int k = 1; k<9; k++) {
+//			System.out.println(7*k);
+//		}
+//		
+//		System.out.println("8, 9단");
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		
+//		if (number < 2) {
+//			System.out.println("입력한 값이 너무 작습니다");
+//		}  else if (number > 9) {
+//			System.out.println("입력한 값이 너무 큽니다");
+//		} else {
+//		for (int k = 1; k<9; k++) {
+//	       System.out.println(number*k);
+//		} 
+//		}
+//		
+//		
 		
 
 	}
