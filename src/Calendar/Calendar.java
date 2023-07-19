ae084f0e83f57f62d8182d0d68d4dd10fe7f6c02
naraeha <dkfmrhs170@gmail.com>
2023-07-19 18:32:33 +0900

    2023 7 19

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -4,57 +4,35 @@ import java.util.Scanner;
 
 public class Calendar {
 	
+	public void sampleCalendar() {
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("----------------");
+		System.out.println("1 2 3 4 5 6 7");
+		System.out.println("8 9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		
+	}
+	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	public int maxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
 
 	public static void main(String[] args) {
-//		System.out.println("일 월 화 수 목 금 토");
-//		System.out.println("----------------");
-//		System.out.println("1 2 3 4 5 6 7");
-//		System.out.println("8 9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 		
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		
-		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-//		String month = scanner.nextLine();
-//		
-//		//존나 무식하게 해보기
-//		if (month.equals("1월")) {
-//			System.out.println("1월은 31일까지 있습니다.");
-//		} else if (month.equals("2월")) {
-//			System.out.println("2월은 28/29일까지 있습니다.");
-//		} else if (month.equals("3월")) {
-//			System.out.println("3월은 31일까지 있습니다.");
-//		}else if (month.equals("4월")) {
-//			System.out.println("4월은 30일까지 있습니다.");
-//		}else if (month.equals("5월")) {
-//			System.out.println("5월은 31일까지 있습니다.");
-//		}else if (month.equals("6월")) {
-//			System.out.println("6월은 30일까지 있습니다.");
-//		}else if (month.equals("7월")) {
-//			System.out.println("7월은 31일까지 있습니다.");
-//		}else if (month.equals("8월")) {
-//			System.out.println("8월은 31일까지 있습니다.");
-//		}else if (month.equals("9월")) {
-//			System.out.println("9월은 30일까지 있습니다.");
-//		}else if (month.equals("10월")) {
-//			System.out.println("10월은 31일까지 있습니다.");
-//		}else if (month.equals("11월")) {
-//			System.out.println("11월은 30일까지 있습니다.");
-//		}else {
-//			System.out.println("12월은 31일까지 있습니다.");
-//		}
-		
+		System.out.println("반복횟수를 입력하세요.");
+		int times = scanner.nextInt();
+			
+		for (int i = 0; i<times; i++) {
+		System.out.println("월을 입력하세요.");
 		int month = scanner.nextInt();
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
 		System.out.printf("%d월은 %d일까지 있습니다.\n",month, cal.maxDaysOfMonth(month));
+		}
+		
+		System.out.println("프로그램 종료");
 		scanner.close();
 
 	}
---
+++ src/Calendar/CalendarSample.java
@@ -0,0 +1,37 @@
+package Calendar;
+
+public class CalendarSample {
+
+	public static void main(String[] args) {
+//		String month = scanner.nextLine();
+//		
+//		//존나 무식하게 해보기
+//		if (month.equals("1월")) {
+//			System.out.println("1월은 31일까지 있습니다.");
+//		} else if (month.equals("2월")) {
+//			System.out.println("2월은 28/29일까지 있습니다.");
+//		} else if (month.equals("3월")) {
+//			System.out.println("3월은 31일까지 있습니다.");
+//		}else if (month.equals("4월")) {
+//			System.out.println("4월은 30일까지 있습니다.");
+//		}else if (month.equals("5월")) {
+//			System.out.println("5월은 31일까지 있습니다.");
+//		}else if (month.equals("6월")) {
+//			System.out.println("6월은 30일까지 있습니다.");
+//		}else if (month.equals("7월")) {
+//			System.out.println("7월은 31일까지 있습니다.");
+//		}else if (month.equals("8월")) {
+//			System.out.println("8월은 31일까지 있습니다.");
+//		}else if (month.equals("9월")) {
+//			System.out.println("9월은 30일까지 있습니다.");
+//		}else if (month.equals("10월")) {
+//			System.out.println("10월은 31일까지 있습니다.");
+//		}else if (month.equals("11월")) {
+//			System.out.println("11월은 30일까지 있습니다.");
+//		}else {
+//			System.out.println("12월은 31일까지 있습니다.");
+//		}
+
+	}
+
+}
