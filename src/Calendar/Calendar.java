92f368816fd6e22c501572c08fe131f8956fc728
naraeha <dkfmrhs170@gmail.com>
2023-07-19 17:19:06 +0900

    Update Calendar.java

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -15,34 +15,40 @@ public class Calendar {
 		
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
-		String month = scanner.nextLine();
+//		String month = scanner.nextLine();
+//		
+//		//존나 무식하게 해보기
+//		if (month.equals("1월")) {
+//			System.out.println("1월은 31일까지 있습니다.");
+//		} else if (month.equals("2월")) {
+//			System.out.println("2월은 28/29일까지 있습니다.");
+//		} else if (month.equals("3월")) {
+//			System.out.println("3월은 31일까지 있습니다.");
+//		}else if (month.equals("4월")) {
+//			System.out.println("4월은 30일까지 있습니다.");
+//		}else if (month.equals("5월")) {
+//			System.out.println("5월은 31일까지 있습니다.");
+//		}else if (month.equals("6월")) {
+//			System.out.println("6월은 30일까지 있습니다.");
+//		}else if (month.equals("7월")) {
+//			System.out.println("7월은 31일까지 있습니다.");
+//		}else if (month.equals("8월")) {
+//			System.out.println("8월은 31일까지 있습니다.");
+//		}else if (month.equals("9월")) {
+//			System.out.println("9월은 30일까지 있습니다.");
+//		}else if (month.equals("10월")) {
+//			System.out.println("10월은 31일까지 있습니다.");
+//		}else if (month.equals("11월")) {
+//			System.out.println("11월은 30일까지 있습니다.");
+//		}else {
+//			System.out.println("12월은 31일까지 있습니다.");
+//		}
 		
-		//존나 무식하게 해보기
-		if (month.equals("1월")) {
-			System.out.println("1월은 31일까지 있습니다.");
-		} else if (month.equals("2월")) {
-			System.out.println("2월은 28/29일까지 있습니다.");
-		} else if (month.equals("3월")) {
-			System.out.println("3월은 31일까지 있습니다.");
-		}else if (month.equals("4월")) {
-			System.out.println("4월은 30일까지 있습니다.");
-		}else if (month.equals("5월")) {
-			System.out.println("5월은 31일까지 있습니다.");
-		}else if (month.equals("6월")) {
-			System.out.println("6월은 30일까지 있습니다.");
-		}else if (month.equals("7월")) {
-			System.out.println("7월은 31일까지 있습니다.");
-		}else if (month.equals("8월")) {
-			System.out.println("8월은 31일까지 있습니다.");
-		}else if (month.equals("9월")) {
-			System.out.println("9월은 30일까지 있습니다.");
-		}else if (month.equals("10월")) {
-			System.out.println("10월은 31일까지 있습니다.");
-		}else if (month.equals("11월")) {
-			System.out.println("11월은 30일까지 있습니다.");
-		}else {
-			System.out.println("12월은 31일까지 있습니다.");
-		}
+		int month = scanner.nextInt();
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n",month, maxDays[month-1]);
+		
 
 	}
 
