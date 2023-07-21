cf337f23f831a202664de3c02fc92ce1557feac0
naraeha <dkfmrhs170@gmail.com>
2023-07-21 17:48:21 +0900

    2023 7 21

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -20,32 +20,37 @@ public class Calendar {
 	}
 
 	
-	public void sampleCalendar(int year, int month) {
+	public void sampleCalendar(int year, int month, int weekday) {
 		System.out.printf("<<%4d년%3d월>>\n",year,month);
-		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("SU MO TU WD TH FR SA");
 		System.out.println("--------------------------");
 		
-		
+		for (int i = 0; i<weekday; i++) {
+			System.out.print("   ");
+		}
 		int maxDay = maxDaysOfMonth(year, month);
+		int count =  7 - weekday;
+		int daylimit = count;
 		
-			for (int i = 1; i<=maxDay; i++) {
-				System.out.printf("%3d",i);
-				if (i%7 == 0) {
-					System.out.println();
-				}
-			}
-	
-		
-				
-			
-		
+		//print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d",i);
+		}
 		System.out.println();
-//		System.out.println("1	2	3	4	5	6	7");
-//		System.out.println("8	9	10	11	12	13	14");
-//		System.out.println("15	16	17	18	19	20	21");
-//		System.out.println("22	23	24	25	26	27	28");
 		
-}
+		//print from second line
+		count++;
+		for (int i = count; i<=maxDay; i++) {
+			System.out.printf("%3d",i);
+			if (i%7 == daylimit) {
+				System.out.println();
+			}
+		}
+			
+		System.out.println();
+		System.out.println();
+
+	}
 	
 	
 	public static void main(String[] args) {
--- src/Calendar/Prompt.java
+++ src/Calendar/Prompt.java
@@ -3,6 +3,29 @@ package Calendar;
 import java.util.Scanner;
 
 public class Prompt {
+		
+	public int parseDay(String week) {
+		if (week.equals("su")) {
+			return 0;
+		} else if (week.equals("mo"))	{
+			return 1;			
+		} else if (week.equals("tu")){ 
+			return 2;			
+		} else if (week.equals("wd")){ 
+			return 3;			
+		} else if (week.equals("th")){ 
+			return 4;			
+		} else if (week.equals("fr")){ 
+			return 5;			
+		} else if (week.equals("sa2")){ 
+			return 6;			
+		} else {
+			return 0;
+		}
+		
+	}
+		 
+	
 	
 	
 	public void runPrompt() {
@@ -11,8 +34,9 @@ public class Prompt {
 		System.out.println("반복횟수를 입력하세요.");
 		
 		int month = 1;	
-		int year = 1;
-		String dOW = null;
+		int year = 0;
+		int weekday = 0;
+		
 		boolean run = true;
 		while (run) {
 		System.out.println("년도를 입력하세요.");
@@ -21,16 +45,20 @@ public class Prompt {
 		System.out.println("월을 입력하세요.");
 		System.out.print("MONTH> ");
 		month = scanner.nextInt();
-		System.out.println("첫번째 요일을 입력하세요.");
+		System.out.println("첫번째 요일을 입력하세요.(su, mo ...");
 		System.out.print("WEEKDAY> ");
-		dOW = scanner.nextLine();
+		String str_weekday = scanner.next();
+		weekday = parseDay(str_weekday);
+		
+		
+		
 		
 		if (month == -1||year == -1) {
 			run = false;
 		} else if (month>12){
 			continue;
 		} else {
-			cal.sampleCalendar(year, month);			
+			cal.sampleCalendar(year, month, weekday);			
 		}
 		}
 		System.out.println("프로그램 종료");
